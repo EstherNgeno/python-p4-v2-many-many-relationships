c6ffc0686ee16eae804c0c2dfe8b14e678c247a1
EstherNgeno <esther.ngeno@student.moringaschool.com>
2024-11-18 17:56:52 +0300

    first commit

--- server/models.py
+++ server/models.py
@@ -10,6 +10,15 @@ metadata = MetaData(naming_convention={
 
 db = SQLAlchemy(metadata=metadata)
 
+employee_meetings = db.Table(
+    'employees_meetings',
+    metadata,
+    db.Column('employee_id', db.Integer, db.ForeignKey(
+        'employees.id'), primary_key=True),
+    db.Column('meeting_id', db.Integer, db.ForeignKey(
+        'meetings.id'), primary_key=True)
+)
+
 
 class Employee(db.Model):
     __tablename__ = 'employees'
@@ -18,6 +27,9 @@ class Employee(db.Model):
     name = db.Column(db.String)
     hire_date = db.Column(db.Date)
 
+    # Relationship mapping the employee to related meetings
+    meetings = db.relationship('Meeting', secondary=employee_meetings, back_populates='employees')
+    
     def __repr__(self):
         return f'<Employee {self.id}, {self.name}, {self.hire_date}>'
 
@@ -30,6 +42,9 @@ class Meeting(db.Model):
     scheduled_time = db.Column(db.DateTime)
     location = db.Column(db.String)
 
+    # Relationship mapping the meeting to related employees
+    employees = db.relationship('Employee', secondary=employee_meetings, back_populates='meetings')
+    
     def __repr__(self):
         return f'<Meeting {self.id}, {self.topic}, {self.scheduled_time}, {self.location}>'
 
